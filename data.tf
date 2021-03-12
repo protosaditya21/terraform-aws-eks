e23623a85eb3eacda3fabafe96ae177be6e665dc
protosaditya21 <39895727+protosaditya21@users.noreply.github.com>
2021-03-12 12:56:46 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -147,10 +147,11 @@ data "template_file" "launch_template_userdata" {
   )
 }
 
-data "aws_iam_role" "custom_cluster_iam_role" {
-  count = var.manage_cluster_iam_resources ? 0 : 1
-  name  = var.cluster_iam_role_name
-}
+#data "aws_iam_role" "custom_cluster_iam_role" {
+#  count = var.manage_cluster_iam_resources ? 0 : 1
+#  name  = var.cluster_iam_role_name
+#}
+
 
 data "aws_iam_instance_profile" "custom_worker_group_iam_instance_profile" {
   count = var.manage_worker_iam_resources ? 0 : local.worker_group_count
