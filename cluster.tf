2ee3160287d5eda8a22a05b1eefeb4a29513ac94
protosaditya21 <39895727+protosaditya21@users.noreply.github.com>
2021-03-15 12:20:17 +0530

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -48,7 +48,7 @@ resource "aws_eks_cluster" "this" {
     aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.cluster_AmazonEKSServicePolicy,
     aws_iam_role_policy_attachment.cluster_AmazonEKSVPCResourceControllerPolicy,
-    aws_cloudwatch_log_group.this
+    aws_cloudwatch_log_group.this,
     aws_iam_role.cluster
   ]
 }
